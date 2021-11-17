51185b3beb2482d0ba0559637aa2cf618a076882
Logan <logan.wm.brantley@gmail.com>
2021-11-17 17:50:31 +1300

    Added test for sell-in

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -10,4 +10,14 @@ describe("Gilded Rose", () => {
     expect(items[0].sellIn).toBe(4);
     expect(items[0].quality).toBe(9);
   });
+
+  it("Past-Sell-In Item Check", () => {
+    const gildedRose = new GildedRose([new Item("Snacks", -1, 10)]);
+
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].name).toBe("Snacks");
+    expect(items[0].sellIn).toBe(-2);
+    expect(items[0].quality).toBe(8);
+  });
 });
